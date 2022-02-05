d7197e689b705e76aca4e329a8fb3ac8c18a2548
Margarita Glushkova <m.glushkova1@gmail.com>
2022-02-05 16:25:55 +0000

    learning DOM

--- 1-exercises/exercise.js
+++ 1-exercises/exercise.js
@@ -3,7 +3,14 @@
   - Install the Live Server plugin
   - Right click on index.html and select "Open with Live Server"
 */
-
+let paragraph = document.querySelectorAll("p");
+console.log(paragraph);
+let classSideHeader = document.querySelector(".site-header");
+console.log(classSideHeader);
+let jumbo = document.querySelector("#jumbotronText");
+console.log(jumbo);
+let primary = document.querySelector(".primary-content").querySelectorAll("p");
+console.log(primary);
 /*
 Task 1
 =======
@@ -22,7 +29,12 @@ Write JavaScript below that console.logs:
     --> should log a list of nodes with a length of 3
 
 */
+let alertBtn = document.querySelector("#alertBtn");
+console.log(alertBtn);
 
+alertBtn.addEventListener("click", () =>
+  alert("Thanks for visiting Bikes for Refugees!")
+);
 
 /*
 Task 2
@@ -30,7 +42,12 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+let changeColorBtn = document.querySelector("#bgrChangeBtn");
 
+changeColorBtn.addEventListener("click", () => {
+  let bodyOfThePage = document.querySelector("body");
+  bodyOfThePage.classList.add("background--red");
+});
 
 /*
 Task 3
@@ -40,14 +57,20 @@ When a user clicks the 'Change colour' button, the background colour of the page
 Hint: can you do this with a CSS class instead of adding styles to the element?
 */
 
-
 /*
 Task 4
 ======
 
 When a user clicks the 'Larger links!' button, the text of all links on the page should increase.
 */
+let largerLinkBtn = document.querySelector("#largerLinksBtn");
 
+largerLinkBtn.addEventListener("click", () => {
+  let links = document.querySelectorAll("a");
+  for (let link of links) {
+    link.style.fontSize = "50px";
+  }
+});
 
 /*
 Task 5
@@ -56,3 +79,14 @@ Task 5
 When a user clicks the 'Add some text' button, the text in the input field should be appended to the page as a new paragraph below it.
 Hint: the new paragraph should be appended to the element with id 'addArticle'.
 */
+
+let addArticleBtn = document.querySelector("#addArticleBtn");
+
+let newP = document.createElement('p');
+let addArticle = document.querySelector("#addArticle");
+addArticleBtn.addEventListener("click", () => {
+  let input = document.querySelector("#addArticleInput").value;
+  newP.innerText = input;
+  addArticle.appendChild(newP)
+});
+
